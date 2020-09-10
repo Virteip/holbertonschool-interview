e99297183a2c5aa2a9ee0de876ee0af4b4d70d53
Virteip <sergiopietri@gmail.com>
2020-09-10 05:36:53 +0000

    [Add]

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -6,17 +6,15 @@
 */
 int check_cycle(listint_t *list)
 {
-listint_t *list_1, *list_2;
-list_1, list_2 = list, list;
-
-while (list_2 && list_2->next)
-{
-list_1 = list_1->next;
-list_2 = list_2->next->next;
-if (list_2 == list_1)
-{
-return (1);
-}
-}
+listint_t *l1, *l2;
+if (!list)
 return (0);
-}
+l1 = list;
+l2 = list;
+while (l1 && l2 && l2->next)
+{
+l1 = l1->next;
+l2 = l2->next->next;
+if (l2 == l1)
+return (1); }
+return (0); }
