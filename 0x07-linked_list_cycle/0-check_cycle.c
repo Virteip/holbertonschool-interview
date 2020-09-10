7d030537822aa999220c03b7c179ac9374b7dc4e
Virteip <sergiopietri@gmail.com>
2020-09-10 05:13:06 +0000

    [Add]

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -1,22 +1,22 @@
 #include "lists.h"
 /**
- *check_cycle - check function
- *@list: linked list pointer to header
- *Return: 1 if true, 0 if null
- */
+*check_cycle - check function
+*@list: linked list pointer to header
+*Return: 1 if true, 0 if null
+*/
 int check_cycle(listint_t *list)
 {
-    listint_t *list_1, *list_2;
-    list_1, list_2 = list, list;
+listint_t *list_1, *list_2;
+list_1, list_2 = list, list;
 
-    while (list_2 && list_2->next)
-    {
-        list_1 = list_1->next;
-        list_2 = list_2->next->next;
-        if (list_2 == list_1)
-        {
-            return (1);
-        }
-    }
-    return (0);
+while (list_2 && list_2->next)
+{
+list_1 = list_1->next;
+list_2 = list_2->next->next;
+if (list_2 == list_1)
+{
+return (1);
+}
+}
+return (0);
 }
